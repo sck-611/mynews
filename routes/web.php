7349580e152e1c3ecfa7d722b7b221973b69247c
EC2 Default User <ec2-user@ip-172-31-5-161.us-east-2.compute.internal>
2021-04-08 17:55:56 +0000

    テスト追加

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 }); 
+
+テスト追加しました。
