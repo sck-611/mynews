9949b9aa8f0b3f3207e08ec3609222a628bd0a2b
EC2 Default User <ec2-user@ip-172-31-5-161.us-east-2.compute.internal>
2021-04-28 22:43:50 +0000

    PHP19課題

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,131 +1,33 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\HTML;
 
-// 以下を追記することでNews Modelが扱えるようになる
+// 追記
 use App\News;
-use App\History;
-
-use Carbon\Carbon;
-
 
 class NewsController extends Controller
 {
-    public function add()
-    {
-        return view('admin.news.create');
-    }
-    
-    public function create(Request $request)
-    {
-        // 以下を追記
-        // Varidationを行う
-        $this->validate($request, News::$rules);
-    
-        $news = new News;
-        $form = $request->all();
-    
-        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-        if (isset($form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
-            dd($path,basename($path));
-        } else {
-            $news->image_path = null;
-        }
-    
-        // フォームから送信されてきた_tokenを削除する
-        unset($form['_token']);
-        // フォームから送信されてきたimageを削除する
-        unset($form['image']);
-    
-        // データベースに保存する
-        $news->fill($form);
-        $news->save();
-    
-        return redirect('admin/news/create');
-    }
-
-
-// // 以下を追記
-//   public function index(Request $request)
-//   {
-//       $cond_title = $request->cond_title;
-//       if ($cond_title != '') {
-//           // 検索されたら検索結果を取得する
-//           $posts = News::where('title', $cond_title)->get();
-//       } else {
-//           // それ以外はすべてのニュースを取得する
-//           $posts = News::all();
-//       }
-//       return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
-//   }
-
-     // 以下を追記
     public function index(Request $request)
     {
         $cond_title = $request->cond_title;
+        // $cond_title が空白でない場合は、記事を検索して取得する
         if ($cond_title != '') {
-            // 検索されたら検索結果を取得する
-            $posts = News::where('title', $cond_title)->get();
+            $posts = News::where('title', $cond_title).orderBy('updated_at', 'desc')->get();
         } else {
-            // それ以外はすべてのニュースを取得する
-            $posts = News::all();
+            $posts = News::all()->sortByDesc('updated_at');
         }
-        return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
 
-}
-
-    public function edit(Request $request)
-    {
-        // News Modelからデータを取得する
-        $news = News::find($request->id);
-        if (empty($news)) {
-            abort(404);    
+        if (count($posts) > 0) {
+            $headline = $posts->shift();
+        } else {
+            $headline = null;
         }
-        return view('admin.news.edit', ['news_form' => $news]);
+
+     // news/index.blade.php ファイルを渡している
+     // また View テンプレートに headline、 posts、 cond_title という変数を渡している
+        return view('news.index', ['headline' => $headline, 'posts' => $posts, 'cond_title' => $cond_title]);
     }
-
-
-    public function update(Request $request)
-    {
-        // Validationをかける
-        $this->validate($request, News::$rules);
-        // News Modelからデータを取得する
-        $news = News::find($request->id);
-        // 送信されてきたフォームデータを格納する
-        $news_form = $request->all();
-        if (isset($news_form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
-            unset($news_form['image']);
-        } elseif (isset($request->remove)) {
-            $news->image_path = null;
-            unset($news_form['remove']);
-        }
-        unset($news_form['_token']);
-        // 該当するデータを上書きして保存する
-        $news->fill($news_form)->save();
-        
-                
-        $history = new History;
-        $history->news_id = $news->id;
-        $history->edited_at = Carbon::now();
-        $history->save();
-
-        return redirect('admin/news');
-    }
-
-    public function delete(Request $request)
-    {
-        // 該当するNews Modelを取得
-        $news = News::find($request->id);
-        // 削除する
-        $news->delete();
-
-
-        return redirect('admin/news/');
-    } 
+} 
